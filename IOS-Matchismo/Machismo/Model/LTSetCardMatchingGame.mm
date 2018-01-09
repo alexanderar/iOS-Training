0624136014c8cd0c70c569e5a277f07461f88a5c
alexanderar <aartyomov@lightricks.com>
2018-01-09 10:38:30 +0200

    SetGame - Changed matching implementation to use dictionaries that count number of matches per property

--- IOS-Matchismo/Machismo/Model/LTSetCardMatchingGame.mm
+++ IOS-Matchismo/Machismo/Model/LTSetCardMatchingGame.mm
@@ -27,53 +27,72 @@ NS_ASSUME_NONNULL_BEGIN
   int score = 0;
   if(cards && cards.count > 0)
   {
-    LTSetCard *firstCard = cards[0];
-    BOOL matchByShade = [LTSetCardMatchingGame matchCards:cards
-                                              byPredicate:[LTSetCardMatchingGame shadeMatchPredicateForCard:firstCard]];
-    BOOL matchByShape = [LTSetCardMatchingGame matchCards:cards
-                                              byPredicate:[LTSetCardMatchingGame shapeMatchPredicateForCard:firstCard]];
-    BOOL matchByNumber = [LTSetCardMatchingGame matchCards:cards
-                                               byPredicate:[LTSetCardMatchingGame numberMatchPredicateForCard:firstCard]];
-    BOOL matchByColor =  [LTSetCardMatchingGame matchCards:cards
-                                               byPredicate:[LTSetCardMatchingGame colorMatchPredicateForCard:firstCard]];
+    BOOL matchByShade = [LTSetCardMatchingGame colorMatch:cards];
+    BOOL matchByShape = [LTSetCardMatchingGame shadeMatch:cards];
+    BOOL matchByNumber = [LTSetCardMatchingGame shapeMatch:cards];
+    BOOL matchByColor =  [LTSetCardMatchingGame numberMatch:cards];
     score =  matchByShade && matchByShape && matchByNumber && matchByColor ? 5 :0;
-
   }
   return score;
 }
 
-+ (NSPredicate *)shapeMatchPredicateForCard:(LTSetCard *)card {
-
-  return [NSPredicate predicateWithBlock:
-          ^BOOL(LTSetCard *otherCard, NSDictionary<NSString *,id> * _Nullable bindings) {
-            return [card.shape isEqualToString:otherCard.shape];
-          }];;
++ (BOOL)colorMatch:(NSArray *)cards {
+  NSMutableDictionary<UIColor*, NSNumber*> *colorMatches = [[NSMutableDictionary alloc]init];
+  for (UIColor *color in  LTSetCard.validColors) {
+    colorMatches[color] = @0;
+  }
+  for (LTSetCard *card in cards) {
+    int currentValue = (int)[colorMatches[card.color] integerValue];
+    colorMatches[card.color] =  [NSNumber numberWithInt:(currentValue + 1)];
+  }
+  return [LTSetCardMatchingGame validateMatchOnDictionary:colorMatches];
 }
 
-+ (NSPredicate *)shadeMatchPredicateForCard:(LTSetCard *)card {
-  return  [NSPredicate predicateWithBlock:
-           ^BOOL(LTSetCard *otherCard, NSDictionary<NSString *,id> * _Nullable bindings) {
-             return (int)card.shade == (int)otherCard.shade;
-           }];
++ (BOOL)shadeMatch:(NSArray *)cards {
+  NSMutableDictionary<NSNumber*, NSNumber*> *shadeMatches =
+      [[NSMutableDictionary alloc]initWithDictionary: @{
+          [NSNumber numberWithInt:LTSetCardShadeOpen]:@0,
+          [NSNumber numberWithInt:LTSetCardShadeStriped]:@0,
+          [NSNumber numberWithInt:LTSetCardShadeSolid]:@0
+      }];
+  for (LTSetCard *card in cards) {
+    int currentValue = (int)[shadeMatches[@((int)card.shade)] integerValue];
+    shadeMatches[@((int)card.shade)] =  [NSNumber numberWithInt:(currentValue + 1)];
+  }
+  return [LTSetCardMatchingGame validateMatchOnDictionary:shadeMatches];
 }
 
-+ (NSPredicate *)numberMatchPredicateForCard:(LTSetCard *)card {
-  return [NSPredicate predicateWithBlock:
-          ^BOOL(LTSetCard *otherCard, NSDictionary<NSString *,id> * _Nullable bindings) {
-            return card.number == otherCard.number;
-          }];
++ (BOOL)shapeMatch:(NSArray *)cards {
+  NSMutableDictionary<NSString*, NSNumber*> *shapeMatches = [[NSMutableDictionary alloc]init];
+  for (NSString *shape in  LTSetCard.validShapes) {
+    shapeMatches[shape] = @0;
+  }
+  for (LTSetCard *card in cards) {
+    int currentValue = (int)[shapeMatches[card.shape] integerValue];
+    shapeMatches[card.shape] =  [NSNumber numberWithInt:(currentValue + 1)];
+  }
+  return [LTSetCardMatchingGame validateMatchOnDictionary:shapeMatches];
 }
 
-+ (NSPredicate *)colorMatchPredicateForCard:(LTSetCard *)card {
-  return [NSPredicate predicateWithBlock:
-          ^BOOL(LTSetCard *otherCard, NSDictionary<NSString *,id> * _Nullable bindings) {
-            return [card.color isEqual:otherCard.color];
-          }];
++ (BOOL)numberMatch:(NSArray *)cards {
+  NSMutableDictionary<NSNumber*, NSNumber*> *numberMatches = [[NSMutableDictionary alloc]init];
+  for (int i = 1; i<=LTSetCard.maxNumber; i++) {
+    numberMatches[@(i)] = @0;
+  }
+  for (LTSetCard *card in cards) {
+    int currentValue = (int)[numberMatches[@(card.number)] integerValue];
+    numberMatches[@(card.number)] =  [NSNumber numberWithInt:(currentValue + 1)];
+  }
+  return [LTSetCardMatchingGame validateMatchOnDictionary:numberMatches];
 }
 
-+ (BOOL)matchCards:(NSArray *)cards byPredicate:(NSPredicate *) predicate{
-  NSArray *filteredArray = [cards filteredArrayUsingPredicate:predicate];
-  return filteredArray.count == 1 || filteredArray.count == cards.count;
++ (BOOL)validateMatchOnDictionary:(NSDictionary *)dictionary {
+  for (id key in dictionary) {
+    if ([dictionary[key] integerValue] == 2) {
+      return NO;
+    }
+  }
+  return YES;
 }
 
 - (NSUInteger)allowedNumberOfChosenCards {
