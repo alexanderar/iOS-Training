6155edaaae9664a85fb4ef2b62308dd6175e14b6
alexanderar <aartyomov@lightricks.com>
2018-01-23 14:26:54 +0200

    Add documentation to LTCardGameConfigProviderProtocol and to LTCardMatcherProtocol.

--- IOS-Matchismo/Machismo/Model/LTCardGameConfigProviderProtocol.h
+++ IOS-Matchismo/Machismo/Model/LTCardGameConfigProviderProtocol.h
@@ -8,15 +8,20 @@
 
 #import "LTCardMatcherProtocol.h"
 #import "LTDeck.h"
+/// Protocol that defines functionality of configuration provider for any card mtching game.
+@protocol LTCardGameConfigProviderProtocol <NSObject>
 
-@protocol LTCardGameConfigProviderProtocol
-
+/// Defines number of chosen cards that should trigger card matching logic.
 @property (readonly, nonatomic) NSUInteger allowedNumberOfChosenCards;
 
+/// Defines initail number of cards that shud be used in the game.
 @property (readonly, nonatomic) NSUInteger initialCardCount;
 
+/// Defines the card matcher which contains all the logic of mathching specific sets of cards per
+/// game type.
 @property (readonly, nonatomic) id <LTCardMatcherProtocol> matcher;
 
+/// Creates new deck of cards.
 - (LTDeck *)createDeck;
 
 @end
--- IOS-Matchismo/Machismo/Model/LTCardMatcherProtocol.h
+++ IOS-Matchismo/Machismo/Model/LTCardMatcherProtocol.h
@@ -6,10 +6,14 @@
 //  Copyright © 2018 Lightricks. All rights reserved.
 //
 
-
 #import "LTCard.h"
-@protocol LTCardMatcherProtocol
 
+/// Prorocol that defines the functionality of card matching that has to be used in card matching
+/// games.
+@protocol LTCardMatcherProtocol  <NSObject>
+
+/// Returns a maching score for the given \c cards array. If there is a match the score is positive,
+/// otherwise it is set to 0.
 - (int)match:(NSArray<LTCard *> *)cards;
 
 @end
